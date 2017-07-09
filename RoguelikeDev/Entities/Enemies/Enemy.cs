bdd5ef10ada9d6998e3287ae68fb33c9de09a940
Graham Marlow <mgmarlow@gmail.com>
2017-07-09 09:36:18 -0700

    Add base enemy and enemy spawner classes

---
+++ RoguelikeDev/Entities/Enemies/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeDev.Entities.Enemies
+{
+    public class Enemy : Sprite
+    {
+        public Enemy(Texture2D texture, Vector2 location)
+        {
+            SpriteTexture = texture;
+            Location = location;
+        }
+
+        public override void Load(ContentManager content)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
---
+++ RoguelikeDev/Entities/Enemies/EnemySpawner.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using RoguelikeDev.Services;
+using RoguelikeDev.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoguelikeDev.Entities.Enemies
+{
+    public class EnemySpawner : IGameObject
+    {
+        private List<Enemy> _badDudes;
+        private Texture2D _texture;
+        private IDungeonMap _dungeon;
+
+        public EnemySpawner ()
+        {
+            _dungeon = ServiceLocator<IDungeonMap>.GetService();
+        }
+
+        public void Load (ContentManager content)
+        {
+            _texture = content.Load<Texture2D>("bad_dude");   
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var enemy in _badDudes)
+            {
+                enemy.Draw(spriteBatch);
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (var enemy in _badDudes)
+            {
+                enemy.Update(gameTime);
+            }
+        }
+
+        public void SpawnNewEnemy()
+        {
+            var map = _dungeon.GetMap();
+            //foreach (var cell in map.GetAllCells())
+            //{
+            //    if (cell.IsWalkable && !cell.IsInFov)
+            //}
+            // TODO: Get random location
+            Vector2 loc = Vector2.Zero;
+            SpawnEnemyAtLocation(loc);
+        }
+
+        public void SpawnEnemyAtLocation(Vector2 location)
+        {
+            _badDudes.Add(new Enemy(_texture, location));
+        }
+
+    }
+}
